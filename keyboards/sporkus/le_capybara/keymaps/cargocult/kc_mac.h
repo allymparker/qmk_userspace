d4e37ac2cbf25a5292a98c1be4d4ab7f886cdb3c
Ally Parker <ally@theparkers.xyz>
2025-09-21 17:45:51 +0100

    Update nav and symbol layers

---
+++ keyboards/sporkus/le_capybara/keymaps/cargocult/kc_mac.h
@@ -0,0 +1,29 @@
+
+#define MC_PND LALT(KC_3)
+#define MC_ZIN LGUI(KC_PLUS)
+#define MC_ZOT LGUI(KC_MINUS)
+#define MC_PSC LSFT(LGUI(LCTL(KC_4)))
+
+// Cut, copy, paste, undo
+#define MC_CUT LGUI(KC_X)
+#define MC_CPY LGUI_T(LGUI(KC_C))
+#define MC_PST LSFT_T(LGUI(KC_V))
+#define MC_UND LGUI(KC_Z)
+
+// Home row mods
+#define MC_A LCTL_T(KC_A)
+#define MC_S LALT_T(KC_S)
+#define MC_D LGUI_T(KC_D)
+#define MC_F LSFT_T(KC_F)
+#define MC_J RSFT_T(KC_J)
+#define MC_K RGUI_T(KC_K)
+#define MC_L RALT_T(KC_L)
+#define MC_SEMI RCTL_T(KC_SEMICOLON)
+
+// Navigation keys
+#define MC_DWD LALT(KC_BSPC)
+#define MC_PWD LALT(KC_LEFT)
+#define MC_NWD LALT(KC_RIGHT)
+#define MC_SPT LGUI(KC_SPC)
+#define MC_HME LGUI(KC_LEFT)
+#define MC_END LGUI(KC_RIGHT)
--- keyboards/sporkus/le_capybara/keymaps/cargocult/keymap.c
+++ keyboards/sporkus/le_capybara/keymaps/cargocult/keymap.c
@@ -4,6 +4,7 @@
 
 #include QMK_KEYBOARD_H
 #include "../../rgb.c"
+#include "kc_mac.h"
 
 // Layer definitions
 enum layers {
@@ -25,14 +26,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      // BASE LAYER (QWERTY)
           [BASE] = LAYOUT_3thumb(
                KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MPLY,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-               KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
+               MC_A,    MC_S,    MC_D,    MC_F,    KC_G,                      KC_H,    MC_J,    MC_K,    MC_L,    MC_SEMI,
                KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                                KC_TAB,  TT_NUM,  TT_NAV,                   TT_FKEY,  TT_SYM,    KC_DEL
           ),
 
      // NUMPAD LAYER
      [NUMPAD] = LAYOUT_3thumb(
-          _______,  _______,  _______,  _______,  _______,    KC_MPLY,         KC_ASTR,   KC_7,   KC_8,   KC_9,   _______,
+          _______,  _______,  _______,  _______,  _______,    _______,         KC_ASTR,   KC_7,   KC_8,   KC_9,   _______,
           _______,  _______,  _______,  _______,  _______,                     KC_PLUS,   KC_4,   KC_5,   KC_6,   _______,
           _______,  _______,  _______,  _______,  _______,                     KC_MINS,   KC_1,   KC_2,   KC_3,   KC_PERC,
                               _______,  _______,  _______,                     KC_EQL,    KC_0, _______
@@ -40,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      // NAVIGATION LAYER (vim style)
      [NAV] = LAYOUT_3thumb(
-           _______,  _______,  _______,  _______,  _______,   KC_MPLY,        _______,   _______,   _______,   _______,   _______,
-           _______,  _______,  _______,  _______,  _______,                   KC_LEFT,     KC_UP,   KC_DOWN,   KC_RGHT,   _______,
-           _______,  _______,  _______,  _______,  _______,                   _______,   _______,   _______,   _______,   _______,
-                               _______,  _______,  _______,                   _______,   _______,   _______
+           _______,  _______,  _______,  _______,  _______,   _______,        _______,    MC_PWD,    MC_NWD,   _______,   _______,
+            MC_UND,   MC_CUT,   MC_CPY,   MC_PST,  _______,                   KC_LEFT,     KC_UP,   KC_DOWN,   KC_RGHT,   _______,
+           _______,  _______,  _______,  _______,  _______,                   _______,    MC_HME,    MC_END,   _______,   _______,
+                               _______,  _______,  _______,                    MC_DWD,    MC_SPT,   _______
      ),
 
      // FKEY LAYER
      [FKEY] = LAYOUT_3thumb(
-           _______,  KC_F9,     KC_F10,   KC_F11,   KC_F12,     _______,       _______,   _______,   _______,   _______,   _______,
+           _______,  KC_F9,     KC_F10,   KC_F11,   KC_F12,     _______,      _______,   _______,   _______,   _______,   _______,
            _______,  KC_F5,      KC_F6,    KC_F7,    KC_F8,                   _______,   _______,   _______,   _______,   _______,
            _______,  KC_F1,      KC_F2,    KC_F3,    KC_F4,                   _______,   _______,   _______,   _______,   _______,
                                _______,  _______,  _______,                   _______,   _______,   _______
@@ -57,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      // SYMBOL LAYER
      [SYM] = LAYOUT_3thumb(
-          KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,         KC_MPLY,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-          KC_UNDS, KC_PLUS, KC_MINS, KC_EQL,  KC_GRV,                    KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_PIPE,
-          KC_LT,   KC_GT,   KC_SCLN, KC_COLN, KC_QUES,                   KC_TILD, KC_BSLS, KC_SLSH, KC_DQUO, KC_QUOT,
+          KC_CIRC, KC_AMPR, KC_HASH, KC_TILD, KC_LPRN,         _______,      KC_RPRN, _______, _______, _______, _______,
+          KC_GRV,  KC_PIPE, KC_EXLM, KC_QUOT, KC_LCBR,                       KC_RCBR, _______, _______, _______, _______,
+          KC_BSLS, KC_AT,   KC_DLR,   MC_PND, KC_LBRC,                       KC_RBRC, _______, _______, _______, _______,
                               _______, _______, _______,                     _______, _______, _______
      ),
      
