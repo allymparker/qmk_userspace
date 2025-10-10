0b504d51f0c496eabbcd63fd13199ec66d484ac4
Ally Parker <ally@theparkers.xyz>
2025-10-10 11:03:56 +0100

    UPdate keymap

--- keyboards/sporkus/le_capybara/keymaps/cargocult/keymap.c
+++ keyboards/sporkus/le_capybara/keymaps/cargocult/keymap.c
@@ -25,34 +25,36 @@ enum layers {
 
 #include "combos.h"
 
+#define LT_NUM LT(NUMPAD, KC_ENT)
+#define LT_NAV LT(NAV, KC_ESC)
+#define LT_FKEY LT(FKEY, KC_BSPC)
+#define LT_SYM LT(SYM, KC_SPC)
 
-// Keymap using LAYOUT_3thumb
+#define LT_V LT(FWD, KC_V)
+#define LT_M LT(BCK, KC_M)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     // BASE LAYER (QWERTY)
-          [BASE] = LAYOUT_3thumb(
-               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MPLY,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+               [BASE] = LAYOUT_3thumb(
+               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_MPLY,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
                MC_A,    MC_S,    MC_D,    MC_F,    KC_G,                      KC_H,    MC_J,    MC_K,    MC_L,    MC_SEMI,
                KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                               KC_TAB,  TT_NUM,  TT_NAV,                   TT_FKEY,  TT_SYM,    KC_DEL
+                               KC_TAB,  LT_NUM,  LT_NAV,                      LT_FKEY, LT_SYM,  KC_DEL
           ),
 
-     // NUMPAD LAYER
      [NUMPAD] = LAYOUT_3thumb(
-          _______,  _______,  _______,  _______,  _______,    _______,         KC_ASTR,   KC_7,   KC_8,   KC_9,   _______,
+          QK_BOOT,  _______,  _______,  _______,  _______,      _______,       KC_ASTR,   KC_7,   KC_8,   KC_9,   _______,
           _______,  _______,  _______,  _______,  _______,                     KC_PLUS,   KC_4,   KC_5,   KC_6,   _______,
           _______,  _______,  _______,  _______,  _______,                     KC_MINS,   KC_1,   KC_2,   KC_3,   KC_PERC,
                               _______,  _______,  _______,                     KC_EQL,    KC_0, _______
      ),
 
-     // NAVIGATION LAYER (vim style)
      [NAV] = LAYOUT_3thumb(
-           _______,  _______,  _______,  _______,  _______,   _______,        GUI_TAB,    MC_PWD,    MC_NWD,   _______,   _______,
-            MC_UND,   MC_CUT,   MC_CPY,   MC_PST,  _______,                   KC_LEFT,     KC_UP,   KC_DOWN,   KC_RGHT,   _______,
-           _______,  _______,  _______,  _______,  _______,                   CTL_TAB,    MC_HME,    MC_END,   _______,   _______,
+           _______,  _______,  _______,  _______,  _______,     _______,       GUI_TAB,    MC_PWD,    MC_NWD,   _______,   _______,
+            MC_UND,   MC_CUT,   MC_CPY,   MC_PST,  _______,                    KC_LEFT,     KC_DOWN,   KC_UP,   KC_RGHT,   _______,
+           _______,  _______,  _______,  _______,  _______,                    CTL_TAB,    MC_HME,    MC_END,   _______,   _______,
                                _______,  _______,  _______,                    MC_DWD,    MC_SPT,   _______
      ),
 
-     // FKEY LAYER
      [FKEY] = LAYOUT_3thumb(
            _______,  KC_F9,     KC_F10,   KC_F11,   KC_F12,     _______,      _______,   _______,   _______,   _______,   _______,
            _______,  KC_F5,      KC_F6,    KC_F7,    KC_F8,                   _______,   _______,   _______,   _______,   _______,
@@ -60,13 +62,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                _______,  _______,  _______,                   _______,   _______,   _______
      ),
 
-
-     // SYMBOL LAYER
      [SYM] = LAYOUT_3thumb(
-          KC_CIRC, KC_AMPR, KC_HASH, KC_TILD, KC_LPRN,         _______,      KC_RPRN, _______, _______, _______, _______,
-          KC_GRV,  KC_PIPE, KC_EXLM, KC_QUOT, KC_LCBR,                       KC_RCBR, _______, _______, _______, _______,
-          KC_BSLS, KC_AT,   KC_DLR,   MC_PND, KC_LBRC,                       KC_RBRC, _______, _______, _______, _______,
-                              _______, _______, _______,                     _______, _______, _______
+          KC_CIRC, KC_AMPR, KC_HASH, KC_TILD, KC_LPRN,          _______,      KC_RPRN, _______, _______, _______, _______,
+          KC_GRV,  KC_PIPE, KC_EXLM, KC_QUOT, KC_LCBR,                        KC_RCBR, _______, _______, _______, _______,
+          KC_BSLS, KC_AT,   KC_DLR,   MC_PND, KC_LBRC,                        KC_RBRC, _______, _______, _______, _______,
+                            KC_UNDS, KC_MINS, KC_DQUO,                        _______, _______, _______
+     ),
+     
      ),
      
      // // NAVIGATION LAYER (vim style)
@@ -81,11 +83,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-     [BASE]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-     [NUMPAD] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-     [NAV]    = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-     [FKEY]   = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
-     [SYM]    = { ENCODER_CCW_CW(KC_MPLY, KC_MUTE) }
+    // Default volume control for all layers
+    [0 ... (LAYER_COUNT - 1)] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    // Override specific layers
+    [NUMPAD] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT) }
 };
 #endif
 
